aba69c390d664159fcefa9a9f9f4a8efec5f240f
jmutt78 <jmcintosh@docusites.com>
2019-04-25 07:43:46 -0700

    finished auth

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -10,6 +10,18 @@ const Query = {
       },
       info
     );
+  },
+  user(parent, args, ctx, info) {
+    // check if there is a current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
   }
 };
 
