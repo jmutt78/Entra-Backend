a914384f593b2e00a24e5c145664d894025ee39a
jmutt78 <jmcintosh@docusites.com>
2020-01-03 07:09:52 -0800

    Fixed prisma

--- src/resolvers/Mastery.js
+++ src/resolvers/Mastery.js
@@ -1,5 +1,72 @@
-var _ = require('lodash');
+const Mastery = {
+  async level1(parent, args, ctx, info) {
+    info.cacheControl.setCacheHint({ maxAge: 3600, scope: 'PRIVATE' });
+    const user = await ctx.db.query.users(
+      {
+        where: {
+          id: parent.id
+        }
+      },
+      `{ id
+      points
+  }`
+    );
 
-const Mastery = {};
+    const islevelOne = user[0].points >= 300;
+
+    return islevelOne;
+  },
+  async level2(parent, args, ctx, info) {
+    info.cacheControl.setCacheHint({ maxAge: 3600, scope: 'PRIVATE' });
+    const user = await ctx.db.query.users(
+      {
+        where: {
+          id: parent.id
+        }
+      },
+      `{ id
+      points
+  }`
+    );
+
+    const islevelTwo = user[0].points >= 500;
+
+    return islevelTwo;
+  },
+  async level3(parent, args, ctx, info) {
+    info.cacheControl.setCacheHint({ maxAge: 3600, scope: 'PRIVATE' });
+    const user = await ctx.db.query.users(
+      {
+        where: {
+          id: parent.id
+        }
+      },
+      `{ id
+      points
+  }`
+    );
+
+    const islevelThree = user[0].points >= 1000;
+
+    return islevelThree;
+  },
+  async level4(parent, args, ctx, info) {
+    info.cacheControl.setCacheHint({ maxAge: 3600, scope: 'PRIVATE' });
+    const user = await ctx.db.query.users(
+      {
+        where: {
+          id: parent.id
+        }
+      },
+      `{ id
+      points
+  }`
+    );
+
+    const islevelFour = user[0].points >= 2000;
+
+    return islevelFour;
+  }
+};
 
 module.exports = Mastery;
